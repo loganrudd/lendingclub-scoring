caac259130b7f7cd76171816983412f45e538a74
Logan William Rudd <logan.rudd@kueski.com>
2020-09-24 10:01:11 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='databricks_labs_cicd_templates_sample_project',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CICD PoC',
+    author='Logan Rudd',
+    license='',
+)
